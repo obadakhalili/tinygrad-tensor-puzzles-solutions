09f33162902f67b2ecdcaf01d2811dc8b2c1ca8a
Obada Khalili <obada.akhalili@gmail.com>
2024-07-09 08:44:27 +0300

    fix arange and add ones

---
+++ puzzles/ones.py
@@ -0,0 +1,12 @@
+from tinygrad import Tensor
+from arange import arange
+from where import where
+
+
+def ones(len: int) -> Tensor:
+    # return Tensor([1])._broadcast_to((len,))
+    return (arange(len) >= 0) * 1
+
+
+if __name__ == "__main__":
+    print(ones(10).numpy())
