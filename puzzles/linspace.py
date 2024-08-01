66382084949d2b3f450aa60dcc23371ce72e3ed4
Obada Khalili <obada.akhalili@gmail.com>
2024-08-01 20:14:17 +0300

    add linspace

---
+++ puzzles/linspace.py
@@ -0,0 +1,11 @@
+from tinygrad import Tensor
+from cumsum import cumsum
+from ones import ones
+
+
+def linspace(i: int, j: int, k: int) -> Tensor:
+    return ones(k) * i + (cumsum(ones(k) * 0 + (step := (j - i) / (k - 1))) - step)
+
+
+if __name__ == "__main__":
+    linspace(10, 1, 5)
