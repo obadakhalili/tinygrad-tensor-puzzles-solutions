a6effa41da0dad0219c18bf642d7358226625859
Obada Khalili <obada.akhalili@gmail.com>
2024-08-01 20:49:19 +0300

    repeat

---
+++ puzzles/repeat.py
@@ -0,0 +1,15 @@
+from tinygrad import Tensor
+from ones import ones
+from arange import arange
+
+
+def repeat(a: Tensor, i: int) -> Tensor:
+    if len(a.shape) > 1 or i < 0:
+        raise ValueError(
+            f"tensor must be a vector and i must be a non-negative integer"
+        )
+    return a + (ones(i) * 0)[:, None]
+
+
+if __name__ == "__main__":
+    print(repeat(Tensor([1, 2, 3]), 2).numpy())
