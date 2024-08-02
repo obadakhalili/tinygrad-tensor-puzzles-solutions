f9aa9cd533de0248f2e6273bd50c323f875c18e2
Obada Khalili <obada.akhalili@gmail.com>
2024-08-02 10:46:07 +0300

    heaviside

---
+++ puzzles/heaviside.py
@@ -0,0 +1,13 @@
+from tinygrad import Tensor
+
+
+def scatter_add(a: Tensor, b: Tensor) -> Tensor:
+    if len(a.shape) > 1 or len(a.shape) != len(b.shape):
+        raise ValueError(
+            f"a and b must be 1D tensors of the same length, and j to be a positive integer"
+        )
+    return (a > 0) + (a == 0) * b
+
+
+if __name__ == "__main__":
+    print(scatter_add(Tensor([1, 0, 3, -1, 0]), Tensor([3, 2, 1, 3, 5])).numpy())
